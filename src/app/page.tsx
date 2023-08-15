7b0a553ae4b9344b159df32a7dd0bcb870cb668b
chichoon <chichoon.choi@gmail.com>
2023-08-15 18:43:42 +0900

    feat: :sparkles: navbar 임시로 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,29 +2,40 @@ import Image from 'next/image';
 
 export default function Home() {
   return (
-    <main className='flex flex-col w-screen h-screen relative'>
+    <>
       {/* TODO: NavBar */}
-      <Image
-        src='/knocking_bg.png'
-        alt='knocking background'
-        width={1920}
-        height={1080}
-        className='w-full h-full object-cover'
-      />
-      <div className='absolute top-0 left-0 w-full h-full bg-black bg-opacity-30 flex flex-col justify-center items-center'>
-        <div className='flex flex-col justify-center items-center bg-white px-20 py-12 rounded-md'>
-          <span className='text-text-light-primary text-xl'>
-            Knocking과 함께 더 쉬워진 이력서 쓰러가기
-          </span>
-          <button
-            type='button'
-            className='bg-primary text-white w-40 h-12 mt-10 rounded-md'
-          >
-            {/* TODO: Button */}
-            <span>로그인</span>
-          </button>
+      <nav className='bg-white flex justify-between items-center px-12 py-4'>
+        <span>Knocking</span>
+        <button
+          type='button'
+          className=''
+        >
+          <span>로그인</span>
+        </button>
+      </nav>
+      <main className='flex flex-col w-screen h-full relative'>
+        <Image
+          src='/knocking_bg.png'
+          alt='knocking background'
+          width={1920}
+          height={1080}
+          className='w-full h-full object-cover'
+        />
+        <div className='absolute top-0 left-0 w-full h-full bg-black bg-opacity-30 flex flex-col justify-center items-center'>
+          <div className='flex flex-col justify-center items-center bg-white px-20 py-12 rounded-md'>
+            <span className='text-text-light-primary text-xl'>
+              Knocking과 함께 더 쉬워진 이력서 쓰러가기
+            </span>
+            <button
+              type='button'
+              className='bg-primary text-white w-40 h-12 mt-10 rounded-md hover:bg-secondary transition-colors'
+            >
+              {/* TODO: Button */}
+              <span>로그인</span>
+            </button>
+          </div>
         </div>
-      </div>
-    </main>
+      </main>
+    </>
   );
 }
