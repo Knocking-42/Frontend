2d26fac1f7329e091f6092618ead0846cdb8a793
chichoon <chichoon.choi@gmail.com>
2023-08-20 00:26:52 +0900

    feat: :sparkles: 토스트 목록 수정

--- src/_shared/components/Toast/ToastList.tsx
+++ src/_shared/components/Toast/ToastList.tsx
@@ -2,19 +2,35 @@
 
 import ReactDOM from 'react-dom';
 
+import { Toast } from './Toast';
+import { ToastData } from '@/_shared/types/toast';
+import { useRef } from 'react';
+
 interface Props {
-  text: string;
+  toastList: ToastData[];
+  onRemoveToast: (index: number) => void;
 }
 
-export const ToastWrapper = ({ text }: Props) => {
+export const ToastList = ({ toastList, onRemoveToast }: Props) => {
   const element = document.getElementById('toast-root');
+  const ref = useRef(null);
+
   if (!element) return null;
+
   return ReactDOM.createPortal(
-    <div className='fixed top-0 right-0  w-fit h-fit'>
-      <ul>
-        <li>{text}</li>
-      </ul>
-    </div>,
+    <ul
+      className='fixed top-0 right-0  w-fit h-fit max-h-full'
+      ref={ref}
+    >
+      {toastList.map((toast, index) => (
+        <Toast
+          key={`toast-${index}`}
+          type={toast.type}
+          text={toast.text}
+          onClose={() => onRemoveToast(index)}
+        />
+      ))}
+    </ul>,
     element,
   );
 };
