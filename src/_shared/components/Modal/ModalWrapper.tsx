42a06986ccdc76d8e68c7fa853129fb2e36c3978
chichoon <chichoon.choi@gmail.com>
2023-08-19 16:37:25 +0900

    refactor: :recycle: ModalPortal 없이도 렌더링 되게끔 변경

--- src/_shared/components/Modal/ModalWrapper.tsx
+++ src/_shared/components/Modal/ModalWrapper.tsx
@@ -1,6 +1,6 @@
 import { ReactNode } from 'react';
+import ReactDOM from 'react-dom';
 
-import ModalPortal from './ModalPortal';
 import useClickOutside from '@/_shared/utils/hooks/useClickOutside';
 
 interface Props {
@@ -11,16 +11,17 @@ interface Props {
 export const ModalWrapper = ({ children, onClickOutside }: Props) => {
   const ref = useClickOutside(onClickOutside);
 
-  return (
-    <ModalPortal>
-      <div className='fixed top-0 left-0 bg-black dark:bg-white bg-opacity-10 w-screen h-screen flex items-center justify-center backdrop-blur-sm'>
-        <div
-          ref={ref}
-          className='bg-bg-light dark:bg-bg-dark rounded-sm overflow-hidden'
-        >
-          {children}
-        </div>
+  const element = document.getElementById('modal-root');
+  if (!element) return null;
+  return ReactDOM.createPortal(
+    <div className='fixed top-0 left-0 bg-black dark:bg-white bg-opacity-10 w-screen h-screen flex items-center justify-center backdrop-blur-sm'>
+      <div
+        ref={ref}
+        className='bg-bg-light dark:bg-bg-dark rounded-sm overflow-hidden'
+      >
+        {children}
       </div>
-    </ModalPortal>
+    </div>,
+    element,
   );
 };
