5004d3507d787d3fc4682f000d2ae1c8c0406b71
chichoon <chichoon.choi@gmail.com>
2023-08-19 23:51:15 +0900

    feat: :sparkles: 토스트 컴포넌트 디자인, close 버튼 추가

--- src/_shared/components/Toast/Toast.tsx
+++ src/_shared/components/Toast/Toast.tsx
@@ -1,12 +1,34 @@
+import { cn } from '@/_shared/utils/hooks/cn';
+
 interface Props {
   text: string;
+  type: 'common' | 'success' | 'error' | 'warning';
+  onClose: () => void;
 }
 
-export const Toast = ({ text }: Props) => {
+export const Toast = ({ text, type, onClose }: Props) => {
   return (
-    <li className='px-5 py-2 bg-slate-300'>
-      {/* TODO: design wip */}
-      <span>{text}</span>
+    <li
+      className={cn('px-5 py-2', {
+        'bg-slate-300': type == 'common', // TODO: design wip
+        'bg-success': type == 'success',
+        'bg-danger': type == 'error',
+        'bg-warning': type == 'warning',
+      })}
+    >
+      <span
+        className={cn('text-white', {
+          'text-black': type == 'warning' || type == 'common',
+        })}
+      >
+        {text}
+      </span>
+      <button
+        type='button'
+        onClick={onClose}
+      >
+        {/* TODO: X Icon */}X
+      </button>
     </li>
   );
 };
