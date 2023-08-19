435013bed0427bf040bf246c2d57521e8b2957aa
chichoon <chichoon.choi@gmail.com>
2023-08-19 23:50:35 +0900

    chore: :lipstick: fade-in, fade-out, fade-top-down 등 애니메이션 추가

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,10 +17,24 @@ const config: Config = {
         success: '#22c55e',
         warning: '#facc15',
       },
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      keyframes: {
+        'fade-in': {
+          '0%': { opacity: '0' },
+          '100%': { opacity: '1' },
+        },
+        'fade-out': {
+          '0%': { opacity: '1' },
+          '100%': { opacity: '0' },
+        },
+        'fade-top-down': {
+          '0%': { opacity: '0', transform: 'translateY(-10px)' },
+          '100%': { opacity: '1', transform: 'translateY(0)' },
+        },
+      },
+      animation: {
+        'fade-in': 'fade-in 0.3s ease-out',
+        'fade-out': 'fade-out 0.3s ease-out',
+        'fade-top-down': 'fade-top-down 0.3s ease-out',
       },
     },
   },
