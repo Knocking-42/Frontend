93c8682360f7461a4debdaf4cacb09189d056577
chichoon <chichoon.choi@gmail.com>
2023-08-20 17:39:56 +0900

    feat: :lipstick: fade-out, fade-in 애니메이션 추가 (토스트용)

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -26,15 +26,20 @@ const config: Config = {
           '0%': { opacity: '1' },
           '100%': { opacity: '0' },
         },
-        'fade-top-down': {
-          '0%': { opacity: '0', transform: 'translateY(-10px)' },
-          '100%': { opacity: '1', transform: 'translateY(0)' },
+        'fade-in-left-right': {
+          '0%': { opacity: '0', transform: 'translateX(-50%)' },
+          '100%': { opacity: '1', transform: 'translateX(0)' },
+        },
+        'fade-out-right-left': {
+          '0%': { opacity: '1', transform: 'translateX(0)' },
+          '100%': { opacity: '0', transform: 'translateX(50%)' },
         },
       },
       animation: {
         'fade-in': 'fade-in 0.3s ease-out',
         'fade-out': 'fade-out 0.3s ease-out',
-        'fade-top-down': 'fade-top-down 0.3s ease-out',
+        'fade-in-left-right': 'fade-in-left-right 0.1s ease-in',
+        'fade-out-right-left': 'fade-out-right-left 0.1s ease-out',
       },
     },
   },
