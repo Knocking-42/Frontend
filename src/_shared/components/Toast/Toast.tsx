89fb59119a18c1957ffa69e50e593a69413f0590
chichoon <chichoon.choi@gmail.com>
2023-08-20 17:40:16 +0900

    feat: :sparkles: 토스트 사라지는 애니메이션 추가

--- src/_shared/components/Toast/Toast.tsx
+++ src/_shared/components/Toast/Toast.tsx
@@ -1,5 +1,10 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+
 import { cn } from '@/_shared/utils/hooks/cn';
-import { ToastType } from '@/_shared/types/toast';
+import { ToastType } from './toast.d';
+import { TOAST_DURATION } from './constants';
 
 interface Props {
   text: string;
@@ -7,17 +12,27 @@ interface Props {
 }
 
 export const Toast = ({ text, type }: Props) => {
+  const [isRemoving, setIsRemoving] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsRemoving(true);
+    }, TOAST_DURATION - 100);
+  }, []);
+
   return (
     <li
-      className={cn('px-5 py-2 animate-fade-in', {
+      className={cn('px-5 py-2 w-60', {
         'bg-slate-300': type == 'common', // TODO: design wip
         'bg-success': type == 'success',
         'bg-danger': type == 'error',
         'bg-warning': type == 'warning',
+        'animate-fade-in-left-right': !isRemoving,
+        'animate-fade-out-right-left': isRemoving,
       })}
     >
       <span
-        className={cn('text-white', {
+        className={cn('text-white select-none', {
           'text-black': type == 'warning' || type == 'common',
         })}
       >
