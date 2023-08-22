1148730d986ef10b3b714fbd2482aa98376d3a36
chichoon <chichoon.choi@gmail.com>
2023-08-22 19:57:07 +0900

    feat: :sparkles: 닫을 때 애니메이션 추가 및 onClose useContext로 전달

--- src/_shared/components/Drawer/Drawer.tsx
+++ src/_shared/components/Drawer/Drawer.tsx
@@ -1,15 +1,23 @@
 'use client';
 
-import useClickOutside from '@/_shared/utils/hooks/useClickOutside';
 import { cva } from 'class-variance-authority';
-import {
-  Dispatch,
-  PropsWithChildren,
-  SetStateAction,
-  useEffect,
-  useState,
-} from 'react';
+import { PropsWithChildren, useEffect, useState } from 'react';
 import { twMerge } from 'tailwind-merge';
+
+import useClickOutside from '@/_shared/utils/hooks/useClickOutside';
+import { DrawerContext } from './DrawerContext';
+
+const DrawerWrapperVariants = cva(
+  'fixed top-0 left-0 w-full h-full overflow-hidden bg-black bg-opacity-10',
+  {
+    variants: {
+      closing: {
+        none: 'animate-fade-in',
+        true: 'animate-fade-out',
+      },
+    },
+  },
+);
 
 const DrawerInnerVariants = cva('bg-bg-light fixed', {
   variants: {
@@ -23,6 +31,13 @@ const DrawerInnerVariants = cva('bg-bg-light fixed', {
       bottom: 'bottom-0 w-full animate-slide-up',
       left: 'left-0 h-full animate-slide-right',
     },
+    closing: {
+      none: '',
+      top: 'animate-slide-out-up',
+      right: 'animate-slide-out-right',
+      bottom: 'animate-slide-out-down',
+      left: 'animate-slide-out-left',
+    },
   },
 });
 
@@ -30,35 +45,57 @@ interface Props extends PropsWithChildren {
   placement: 'top' | 'right' | 'bottom' | 'left';
   size: 'sm' | 'md';
   isOpen: boolean;
-  setIsOpen: Dispatch<SetStateAction<boolean>>;
+  onClose: () => void;
 }
 
 export const Drawer = ({
   placement,
   size,
   isOpen,
-  setIsOpen,
+  onClose,
   children,
 }: Props) => {
   const [element, setElement] = useState<HTMLElement | null>(null);
-  const ref = useClickOutside(() => setIsOpen(false));
+  const [isClosing, setIsClosing] = useState(false);
+  const ref = useClickOutside(handleClose);
 
   useEffect(() => {
     setElement(document.getElementById('drawer-root'));
   }, []);
 
+  function handleClose() {
+    setIsClosing(true);
+    setTimeout(() => {
+      setIsClosing(false);
+      onClose();
+    }, 200);
+  }
+
   if (!element || !isOpen) return <></>;
 
   return (
-    <div className='fixed top-0 left-0 w-full h-full overflow-hidden bg-black bg-opacity-10 animate-fade-in'>
-      {isOpen && (
-        <div
-          ref={ref}
-          className={twMerge(DrawerInnerVariants({ placement, size }))}
-        >
-          {children}
-        </div>
-      )}
-    </div>
+    <DrawerContext.Provider value={{ onClose: handleClose }}>
+      {/* TODO: useMemo */}
+      <div
+        className={DrawerWrapperVariants({
+          closing: isClosing ? true : 'none',
+        })}
+      >
+        {isOpen && (
+          <div
+            ref={ref}
+            className={twMerge(
+              DrawerInnerVariants({
+                placement,
+                size,
+                closing: isClosing ? placement : 'none',
+              }),
+            )}
+          >
+            {children}
+          </div>
+        )}
+      </div>
+    </DrawerContext.Provider>
   );
 };
--- src/_shared/components/Drawer/DrawerCloseButton.tsx
+++ src/_shared/components/Drawer/DrawerCloseButton.tsx
@@ -1,14 +1,15 @@
+import { useContext } from 'react';
+
 import { XIcon } from '@/_shared/assets';
+import { DrawerContext } from './DrawerContext';
 
-interface Props {
-  onClose: () => void;
-}
-
-export const DrawerCloseButton = ({ onClose }: Props) => {
+export const DrawerCloseButton = () => {
+  const { onClose } = useContext(DrawerContext);
   return (
     <button
       type='button'
       onClick={onClose}
+      className='w-4 h-4'
     >
       <XIcon />
     </button>
