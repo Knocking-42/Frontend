51d2fc5c72802aaf34239da2e372767422d89881
mike2ox <firefinger07@gmail.com>
2023-08-23 09:15:39 +0900

    refactor: :pencil2: fix typo

--- src/app/development/page.tsx
+++ src/app/development/page.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link';
 
-export default function SheredComponentTestPage() {
+export default function SharedComponentTestPage() {
   return (
     <>
       <Link href='/development/paper'>paper</Link>
