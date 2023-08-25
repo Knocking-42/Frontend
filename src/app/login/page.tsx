67d86cb50787f149d78e1ee978774c4b76c528ce
chichoon <chichoon.choi@gmail.com>
2023-08-25 19:47:23 +0900

    feat: :sparkles: 로그인 붙이기

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -7,7 +7,7 @@ export default function Login() {
     <>
       <NavBar isLogin={false} />
       <LandingLayout>
-        <a href=''>
+        <a href={process.env.NEXT_PUBLIC_SERVER_URL + '/auth/google'}>
           <Image
             src='/google_signin.png'
             alt='google login button'
@@ -16,7 +16,7 @@ export default function Login() {
             className='mb-5'
           />
         </a>
-        <a href=''>
+        <a href={process.env.NEXT_PUBLIC_SERVER_URL + '/auth/kakao'}>
           <Image
             src='/kakao_signin.png'
             alt='kakao login button'
