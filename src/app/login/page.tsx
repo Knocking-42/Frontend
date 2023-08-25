d0b795967e23c0f89c72744f84affbb3b253cb80
chichoon <chichoon.choi@gmail.com>
2023-08-25 17:33:57 +0900

    feat: :sparkles: 로그인 페이지 구성 및 랜딩페이지 레이아웃 적용

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,3 +1,4 @@
+import { LandingLayout } from '@/_shared/components/LandingLayout';
 import { NavBar } from '@/_shared/components/NavBar';
 import Image from 'next/image';
 
@@ -5,29 +6,25 @@ export default function Home() {
   return (
     <>
       <NavBar isLogin={false} />
-      <main className='flex flex-col w-screen h-full relative'>
-        <Image
-          src='/knocking_bg.png'
-          alt='knocking background'
-          width={1920}
-          height={1080}
-          className='w-full h-full object-cover'
-        />
-        <div className='absolute top-0 left-0 w-full h-full bg-black bg-opacity-30 flex flex-col justify-center items-center'>
-          <div className='flex flex-col justify-center items-center bg-white px-20 py-12 rounded-md'>
-            <span className='text-text-light-primary text-xl'>
-              Knocking과 함께 더 쉬워진 이력서 쓰러가기
-            </span>
-            <button
-              type='button'
-              className='bg-primary text-white w-40 h-12 mt-10 rounded-md hover:bg-secondary transition-colors'
-            >
-              {/* TODO: Button */}
-              <span>로그인</span>
-            </button>
-          </div>
-        </div>
-      </main>
+      <LandingLayout>
+        <a href=''>
+          <Image
+            src='/google_signin.png'
+            alt='google login button'
+            width={200}
+            height={120}
+            className='mb-5'
+          />
+        </a>
+        <a href=''>
+          <Image
+            src='/kakao_signin.png'
+            alt='kakao login button'
+            width={200}
+            height={120}
+          />
+        </a>
+      </LandingLayout>
     </>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,22 @@
 import { NavBar } from '@/_shared/components/NavBar';
-import Image from 'next/image';
+import { LandingLayout } from '@/_shared/components/LandingLayout';
 
 export default function Home() {
   return (
     <>
       <NavBar isLogin={false} />
-      <main className='flex flex-col w-screen h-full relative'>
-        <Image
-          src='/knocking_bg.png'
-          alt='knocking background'
-          width={1920}
-          height={1080}
-          className='w-full h-full object-cover'
-        />
-        <div className='absolute top-0 left-0 w-full h-full bg-black bg-opacity-30 flex flex-col justify-center items-center'>
-          <div className='flex flex-col justify-center items-center bg-white px-20 py-12 rounded-md'>
-            <span className='text-text-light-primary text-xl'>
-              Knocking과 함께 더 쉬워진 이력서 쓰러가기
-            </span>
-            <button
-              type='button'
-              className='bg-primary text-white w-40 h-12 mt-10 rounded-md hover:bg-secondary transition-colors'
-            >
-              {/* TODO: Button */}
-              <span>로그인</span>
-            </button>
-          </div>
-        </div>
-      </main>
+      <LandingLayout>
+        <span className='text-text-light-primary text-xl'>
+          Knocking과 함께 더 쉬워진 이력서 쓰러가기
+        </span>
+        <button
+          type='button'
+          className='bg-primary text-white w-40 h-12 mt-10 rounded-md hover:bg-secondary transition-colors'
+        >
+          {/* TODO: Button */}
+          <span>로그인</span>
+        </button>
+      </LandingLayout>
     </>
   );
 }
