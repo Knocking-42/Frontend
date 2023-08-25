7a908436ef0e7ccd216a13893e616599990bc338
chichoon <chichoon.choi@gmail.com>
2023-08-25 17:25:17 +0900

    feat: :sparkles: Navigation Bar 추가 및 공용컴포넌트로 분리

---
+++ src/_shared/components/NavBar/NavBar.tsx
@@ -0,0 +1,24 @@
+interface Props {
+  isLogin: boolean;
+}
+
+export const NavBar = ({ isLogin }: Props) => {
+  return (
+    <nav className='bg-white flex justify-between items-center px-12 py-4'>
+      <span>Knocking</span>
+      {isLogin ? (
+        <div className='flex flex-row items-center'>
+          <button type='button'>내 이력서</button>
+          <button type='button'>내 계정</button>
+        </div>
+      ) : (
+        <button
+          type='button'
+          className=''
+        >
+          <span>로그인</span>
+        </button>
+      )}
+    </nav>
+  );
+};
